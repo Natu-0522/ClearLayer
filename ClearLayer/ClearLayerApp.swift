7333db71b946aadc798b81f3e7cf8b87ac2374bf
Natu-0522 <115112525+Natu-0522@users.noreply.github.com>
2025-05-20 23:56:17 +0900

    Initial Commit

---
+++ ClearLayer/ClearLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearLayerApp.swift
+//  ClearLayer
+//
+//  Created by 中里祐希 on 2025/05/20.
+//
+
+import SwiftUI
+
+@main
+struct ClearLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClearLayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClearLayer
+//
+//  Created by 中里祐希 on 2025/05/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
